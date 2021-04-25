4eb9c4787dd1861b9d943adf734ed7f7ffc56e37
Ty Lovejoy <48333668+tylovejoy@users.noreply.github.com>
2021-04-25 10:59:05 -0500

    acc-bot-1 [FEATURE REQUEST] Welcome Users Remove cog_check. There are no commands in this cog. No checks needed.

--- cogs/greeting.py
+++ cogs/greeting.py
@@ -7,10 +7,6 @@ class Greeting(commands.Cog, name="Greet newcomers."):
     def __init__(self, bot):
         self.bot = bot
 
-    async def cog_check(self, ctx):
-        if ctx.channel.id == constants.WELCOME_CHANNEL_ID:
-            return True
-
     @commands.Cog.listener()
     async def on_member_join(self, member):
         embed = Embed(title=f"Hello, {member.mention}")
