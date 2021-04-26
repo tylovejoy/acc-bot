7aaa911b9469af66869430247e1abef6fbb12eca
nebulaOW <religiossdoge@gmail.com>
2021-04-25 23:44:14 -0500

    acc-bot-1 [FEATURE REQUEST] Welcome Users Add roles reacts and corresponding embed fields. on_raw_reaction_add() stub init

--- cogs/greeting.py
+++ cogs/greeting.py
@@ -9,11 +9,33 @@ class Greeting(commands.Cog, name="Greet newcomers."):
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
-        embed = Embed(title=f"Hello, {member.mention}")
+        embed = Embed(title="Hello!")
         embed.add_field(
-            name="Welcome to the IT & CS ACC Students Server!", value="Placeholder"
+            name="Welcome to the IT & CS ACC Students Server",
+            value="We hope you enjoy your stay. :)",
+            inline=False,
         )
-        await self.bot.get_channel(constants.WELCOME_CHANNEL_ID).send(embed=embed)
+        embed.add_field(
+            name="Role selection",
+            value="React with the roles you want access to!",
+            inline=False,
+        )
+        embed.add_field(name="🔌", value="IT Support", inline=True)
+        embed.add_field(name="⌨", value="Programming", inline=True)
+        embed.add_field(name="🔒", value="Cyber-Security", inline=True)
+        embed.add_field(name="💻", value="Web Development", inline=True)
+        msg = await self.bot.get_channel(constants.WELCOME_CHANNEL_ID).send(
+            f"{member.mention}",
+            embed=embed,
+        )
+        await msg.add_reaction("🔌")
+        await msg.add_reaction("⌨")
+        await msg.add_reaction("🔒")
+        await msg.add_reaction("💻")
+
+    @commands.Cog.listener()
+    async def on_raw_reaction_add(self, payload):
+        pass
 
 
 def setup(bot):
